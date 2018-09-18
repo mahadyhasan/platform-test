2485ce35d096d7323a89c6f15b69203efda175b9
mahadyhasan <mahady.hasan@hotmail.com>
2018-09-18 13:22:35 +0100

    added a section contract builder

--- src/main/kotlin/co/wrisk/platformtest/App.kt
+++ src/main/kotlin/co/wrisk/platformtest/App.kt
@@ -1,27 +1,26 @@
 package co.wrisk.platformtest
 
-import co.wrisk.platformservice.contracts.BundleContract
-import co.wrisk.platformservice.contracts.NamedItemContract
-import co.wrisk.platformservice.contracts.SectionContract
-import co.wrisk.platformtest.model.SectionType
+import co.wrisk.platformtest.builder.sectionContract
 import java.math.BigDecimal
 
 fun main(args: Array<String>) {
 
-    val bundleContract = BundleContract(
-            BigDecimal.valueOf(2500),
-            BigDecimal.valueOf(200)
-    )
+    val sectionContract = sectionContract {
+        name = "Rick Sanchez"
+        sumInsured
+        bundleContract {
+            sumInsured = BigDecimal.valueOf(2500)
+            excess = BigDecimal.valueOf(200)
+        }
+        namedItemContracts {
+            namedItemContract {
+                name = "Portal Gun"
+                sumInsured = BigDecimal.valueOf(2000)
+                excess = BigDecimal.valueOf(300)
+            }
+        }
 
-    val namedItemContract1 = NamedItemContract("Portal Gun",
-            BigDecimal.valueOf(2000),
-            BigDecimal.valueOf(300)
-    )
+    }
 
-
-    val listOfNamedItemContract = listOf(namedItemContract1)
-
-    val sectionContract = SectionContract(SectionType.ELECTRONIC.name, BigDecimal.ZERO, bundleContract, listOfNamedItemContract)
-
-
+    println(sectionContract)
 }
